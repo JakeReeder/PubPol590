ffa6950886bef271f057a521148c06768e06b00c
JakeReeder <Jake@Jakes-MacBook-Pro.local>
2015-01-28 15:28:20 -0500

    Reorganize to remove data and add Class 3 Python

---
+++ JReeder Class 3 -- Cleaning.py
@@ -0,0 +1,8 @@
+## CLASS 3 DATA CLEANING: 01/28/2014
+
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+
+main_dir = "/Users/Jake/Desktop/Data"
+git_dir = "/Users/Jake/Desktop/Duke/Energy and Big Data/PubPol590"
