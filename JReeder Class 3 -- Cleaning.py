f013afedc9574845b839f98a560d5fa4e06913ac
JakeReeder <Jake@Jakes-MacBook-Pro.local>
2015-01-28 15:38:41 -0500

    Updated in class

--- JReeder Class 3 -- Cleaning.py
+++ JReeder Class 3 -- Cleaning.py
@@ -3,6 +3,12 @@
 from pandas import Series, DataFrame
 import pandas as pd
 import numpy as np
+import os
 
-main_dir = "/Users/Jake/Desktop/Data"
+main_dir = "/Users/Jake/Desktop/Duke/Energy and Big Data/Data"
 git_dir = "/Users/Jake/Desktop/Duke/Energy and Big Data/PubPol590"
+csv_file ="sample_data_clean.csv"
+
+#OS Module ------------------------
+
+df = pd.read_csv(os.path.join(main_dir, csv_file))
